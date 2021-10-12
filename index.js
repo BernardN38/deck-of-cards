22cda26bc88d9d927b68b67d999fa8503335f1df
Bernardo Narvaez <85888299+BernardN38@users.noreply.github.com>
2021-10-11 17:42:35 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,93 @@
+// const mainContainter = $('#facts');
+// const button = $('#get-card');
+// let numberUrl = 'http://numbersapi.com/';
+
+// function getNumberFacts(numbers = [ 1, 2, 3 ], type = 'trivia') {
+// 	$.get(`${numberUrl}${[ ...numbers ]}/${type}?json`).then((res) => {
+// 		res = JSON.parse(res);
+// 		for (let fact in res) {
+// 			mainContainter.append(`<p>${res[fact]}</p>`);
+// 		}
+// 	});
+// }
+
+// let cardUrl = 'http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=';
+// let deckId;
+// function getDeck(callback) {
+// 	$.get(`${cardUrl}${(deck_count = 1)}`).done((res) => {
+// 		deckId = res['deck_id'];
+// 		callback();
+// 	});
+// }
+
+// function getCard(deck = deckId) {
+// 	$.get(`http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`).then((res) => {
+//         let imageUrl;
+//         try {
+// 		    imageUrl = res['cards'][0]['image'];
+//             showCard(imageUrl);
+//         } catch (err) {
+//             console.log('done')
+//         }
+// 	});
+// }
+
+// function showCard(imageUrl) {
+// 	let rotation = Math.random() * (30 - -30) + -30;
+// 	mainContainter.append(`<img class='card' style='transform:rotate(${rotation}deg)'src='${imageUrl}'></img>`);
+// }
+
+// function handleClick() {
+// 	if (!deckId) {
+// 		getDeck(getCard);
+// 	} else {
+// 		getCard();
+// 	}
+// }
+
+// $(button).on('click', handleClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const mainContainter = $('#facts');
 const button = $('#get-card');
 let numberUrl = 'http://numbersapi.com/';
 
-function getNumberFacts(numbers = [ 1, 2, 3 ], type = 'trivia') {
-	$.get(`${numberUrl}${[ ...numbers ]}/${type}?json`).then((res) => {
-		res = JSON.parse(res);
+async function getNumberFacts(numbers = [ 1, 2, 3 ], type = 'trivia') {
+	let res = await axios.get(`${numberUrl}${[ ...numbers ]}/${type}?json`)
 		for (let fact in res) {
 			mainContainter.append(`<p>${res[fact]}</p>`);
 		}
-	});
+        console.log(res)
 }
 
 let cardUrl = 'http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=';
 let deckId;
-function getDeck(callback) {
-	$.get(`${cardUrl}${(deck_count = 1)}`).done((res) => {
-		deckId = res['deck_id'];
-		callback();
-	});
+
+async function getDeck() {
+	let res = await axios.get(`${cardUrl}${(deck_count = 1)}`)
+    deckId = res.data.deck_id
 }
 
-function getCard(deck = deckId) {
-	$.get(`http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`).then((res) => {
-        let imageUrl;
-        try {
-		    imageUrl = res['cards'][0]['image'];
-            showCard(imageUrl);
-        } catch (err) {
-            console.log('done')
-        }
-	});
+async function getCard(deck = deckId) {
+	let res = await axios.get(`http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`)
+    return res.data.cards[0]
 }
 
 function showCard(imageUrl) {
@@ -37,11 +95,19 @@ function showCard(imageUrl) {
 	mainContainter.append(`<img class='card' style='transform:rotate(${rotation}deg)'src='${imageUrl}'></img>`);
 }
 
-function handleClick() {
+async function handleClick() {
 	if (!deckId) {
-		getDeck(getCard);
+		await getDeck()
+        let card = await getCard()
+        showCard(card.image)
 	} else {
-		getCard();
+		let card = await getCard()
+        try {
+        showCard(card.image)
+        } 
+        catch {
+            console.log('No more cards')
+        }
 	}
 }
 
